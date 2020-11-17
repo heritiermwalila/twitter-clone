c53850442165f6d778295d9ad2e16bf22aeebdc1
heritier.mbumba <heritier.mbumba@1-grid.com>
2020-11-17 16:07:21 +0200

    register and login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,14 @@ import { validationResult } from "express-validator";
 
 const Login = (req, res, next) => {
   try {
-    res.status(200).render("login", { title: "Login" });
+    let errors = [];
+    if (req.session.errors && req.session.errors.length > 0) {
+      errors = req.session.errors;
+    }
+    setTimeout(() => {
+      req.session.errors = [];
+    }, 3000);
+    res.status(200).render("login", { title: "Login", errors });
   } catch (error) {
     
   }
@@ -10,9 +17,23 @@ const Login = (req, res, next) => {
 
 export const PostLogin = (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    console.log("===========Error");
-    console.log(errors);
+    const { username, password } = req.body;
+    let errors = [];
+    const { errors: validationErrors } = validationResult(req);
+    if (validationErrors.length > 0) {
+      errors = validationErrors;
+    }
+
+    res
+      .status(200)
+      .render("login", { title: "Login", errors, form: { username } });
+    /**
+     * Steps
+     * -------------------
+     * 1. Check the user in the db
+     * 2. Create a session
+     * 3. Redirect to home page
+     */
   } catch (error) {}
 };
 
@@ -26,6 +47,24 @@ const Register = (req, res, next) => {
 
 export const PostRegister = (req, res, next) => {
   try {
+    req.session.errors = null;
+    const { username, email, password, cpassword } = req.body;
+    let errors = [];
+    const { errors: validationErrors } = validationResult(req);
+    if (validationErrors.length > 0) {
+      errors = validationErrors;
+    }
+    if (password !== cpassword) {
+      errors.push({ msg: "Passwords does not match" });
+    }
+
+    res
+      .status(200)
+      .render("register", {
+        title: "Register",
+        errors,
+        form: { username, email },
+      });
   } catch (error) {}
 };
 
--- server.js
+++ server.js
@@ -5,8 +5,17 @@ import publicRoutes from './routes/public.route.'
 import appRoutes from './routes/app.route.'
 const app = express()
 import {join} from 'path'
+import redis from "redis";
+import connectRedis from "connect-redis";
+import session from "express-session";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
 
 const server = http.createServer(app)
+
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
@@ -14,10 +23,18 @@ app.set('views', 'views')
 app.use(express.static(join(__dirname, 'public')))
 
 
-app.use('/auth', publicRoutes)
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "twitter-clone-2020",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+
+app.use("/auth", publicRoutes);
 app.use('/', appRoutes)
-
 
 const PORT = process.env.PORT || 5500
 
