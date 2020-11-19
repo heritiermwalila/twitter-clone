9d9fdd039131ebe558f61aee752b6c7b3b881869
heritier.mbumba <heritier.mbumba@1-grid.com>
2020-11-19 16:21:16 +0200

    addlogout

--- routes/app.route..js
+++ routes/app.route..js
@@ -4,7 +4,17 @@ import { isLoggedIn } from '../middlewares/auth.middleware'
 const route = Router()
 
 route.get('/', isLoggedIn, Home)
-route.get('/profile', Profile)
+route.get("/profile", isLoggedIn, Profile);
+
+route.post("/logout", isLoggedIn, (req, res, next) => {
+  try {
+    req.session.destroy(() => {
+      res.redirect("/auth/login");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 export default route
